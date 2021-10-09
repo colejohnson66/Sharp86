296aeb99064f1934f36e4c4d71631dbf13c44c8e
Cole Johnson <coleharrisjohnson@gmail.com>
2021-10-09 13:19:36 -0400

    Working on the decoder data classes

---
+++ Sharp86/Cpu/Instruction/Instruction.cs
@@ -0,0 +1,94 @@
+/* =============================================================================
+ * File:   Instruction.cs
+ * Author: Cole Tobin
+ * =============================================================================
+ * <TODO: Purpose>
+ * =============================================================================
+ * Copyright (c) 2021 Cole Tobin
+ *
+ * This file is part of Sharp86.
+ *
+ * Sharp86 is free software: you can redistribute it and/or modify it under the
+ *   terms of the GNU General Public License as published by the Free Software
+ *   Foundation, either version 3 of the License, or (at your option) any later
+ *   version.
+ *
+ * Sharp86 is distributed in the hope that it will be useful, but WITHOUT ANY
+ *   WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ *   FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ *   details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ *   Sharp86. If not, see <http://www.gnu.org/licenses/>.
+ * =============================================================================
+ */
+using System;
+using System.Diagnostics.Contracts;
+
+namespace Sharp86.Cpu.Instruction
+{
+    public delegate ExceptionCode? Handler(Cpu cpu, Instruction instr);
+    public class Instruction
+    {
+        // public Handler Handler;
+
+        public int Segment = -1;
+
+        private readonly Mode InitialMode;
+        public bool ASizeOverride = false;
+        public bool OSizeOverride = false;
+
+        public int RepPrefix = -1;
+        public bool LockPrefix = false;
+
+        public ModRM? ModRM = null;
+        public Sib? Sib = null;
+
+        public uint Displacement = 0;
+        public uint Immediate = 0;
+
+        // various misc bits
+        public bool W = false; // 64 bit OSIZE or opcode extension
+        public bool Z = false; // merge masking
+        public bool B = false; // broadcast, round control (with LL), or exception suppression
+        public int LL = -1; // VLen or round control (with B when LIG/LLIG)
+        public int Vvvv = 0; // v' prepended from EVEX
+        public int Kmask = 0; // EVEX.aaaa
+
+        public Instruction(Mode mode)
+        {
+            InitialMode = mode;
+        }
+
+        /// <summary>The effective OSIZE for this instruction</summary>
+        /// <remarks>
+        /// This value is useless for SIMD instructions.
+        /// </remarks>
+        public Mode EffectiveOSize
+        {
+            get
+            {
+                if (InitialMode == Mode.Mode16)
+                    return OSizeOverride ? Mode.Mode32 : Mode.Mode16;
+                else if (InitialMode == Mode.Mode32)
+                    return OSizeOverride ? Mode.Mode16 : Mode.Mode32;
+
+                // 64 bit
+                if (W)
+                    return Mode.Mode64;
+                return OSizeOverride ? Mode.Mode16 : Mode.Mode32;
+            }
+        }
+
+        /// <summary>The effective ASIZE for this instruction</summary>
+        public Mode EffectiveASize
+        {
+            get
+            {
+                if (InitialMode == Mode.Mode16)
+                    return ASizeOverride ? Mode.Mode32 : Mode.Mode16;
+                return ASizeOverride ? Mode.Mode16 : Mode.Mode32;
+            }
+        }
+    }
+}
---
+++ Sharp86/Cpu/Instruction/ModRM_Sib.cs
@@ -0,0 +1,109 @@
+/* =============================================================================
+ * File:   ModRM_Sib.cs
+ * Author: Cole Tobin
+ * =============================================================================
+ * <TODO: Purpose>
+ * =============================================================================
+ * Copyright (c) 2021 Cole Tobin
+ *
+ * This file is part of Sharp86.
+ *
+ * Sharp86 is free software: you can redistribute it and/or modify it under the
+ *   terms of the GNU General Public License as published by the Free Software
+ *   Foundation, either version 3 of the License, or (at your option) any later
+ *   version.
+ *
+ * Sharp86 is distributed in the hope that it will be useful, but WITHOUT ANY
+ *   WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ *   FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ *   details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ *   Sharp86. If not, see <http://www.gnu.org/licenses/>.
+ * =============================================================================
+ */
+
+namespace Sharp86.Cpu.Instruction
+{
+    public class ModRM
+    {
+        // +-------------------------------+
+        // | 7 | 6 | 5 | 4 | 3 | 2 | 1 | 0 |
+        // |  mod  |    reg    |    r/m    |
+        // +-------------------------------+
+        //
+        // `reg` can be extended by R and R' from the various prefixes
+        // `r/m` can be extended by B (and X for SIB-less EVEX) from the various prefixes
+
+        public ModRM(byte source)
+        {
+            Mod = source >> 6;
+            Reg = (source >> 3) & 7;
+            RM = source & 7;
+        }
+
+        public int Mod { get; }
+        public int Reg { get; private set; }
+        public int RM { get; private set; }
+
+        public void AddRegBit3(bool bit)
+        {
+            if (bit)
+                Reg |= 0b1_000;
+        }
+        public void AddRegBit4(bool bit)
+        {
+            if (bit)
+                Reg |= 0b10_000;
+        }
+        public void AddRMBit3(bool bit)
+        {
+            if (bit)
+                RM |= 0b1_000;
+        }
+        public void AddRMBit4(bool bit)
+        {
+            if (bit)
+                RM |= 0b10_000;
+        }
+    }
+
+    public class Sib
+    {
+        // +-------------------------------+
+        // | 7 | 6 | 5 | 4 | 3 | 2 | 1 | 0 |
+        // | scale |   index   |    base   |
+        // +-------------------------------+
+        //
+        // `index` can be extended from X (and v' for VSIB) from the various prefixes
+        // `base` can be extended from B
+        //  - no "bit 4" extension as `base` is always a GPR
+
+        public Sib(byte source)
+        {
+            Scale = source >> 6;
+            Index = (source >> 3) & 7;
+            Base = source & 7;
+        }
+
+        public int Scale { get; }
+        public int Index { get; private set; }
+        public int Base { get; private set; }
+
+        public void AddIndexBit3(bool bit)
+        {
+            if (bit)
+                Index |= 0b1_000;
+        }
+        public void AddIndexBit4(bool bit)
+        {
+            if (bit)
+                Index |= 0b10_000;
+        }
+        public void AddBaseBit3(bool bit)
+        {
+            if (bit)
+                Base |= 0b1_000;
+        }
+    }
+}
