61935e5d10874791cfdf6c4ba9fc6dc094ac8b4c
Cole Johnson <coleharrisjohnson@gmail.com>
2021-10-24 15:48:17 -0400

    Use `RaiseException` instead of returning one

--- Sharp86/Cpu/Instruction/Error.cs
+++ Sharp86/Cpu/Instruction/Error.cs
@@ -28,5 +28,8 @@
 namespace Sharp86.Cpu.Instruction;
 public static class Error
 {
-    public static CpuExceptionCode? _(CpuCore cpu, Instruction instr) => CpuExceptionCode.UD;
+    public static void _(CpuCore cpu, Instruction instr)
+    {
+        cpu.RaiseException(CpuExceptionCode.UD);
+    }
 }
